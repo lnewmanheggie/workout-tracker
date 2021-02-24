3ee222c4249b161a4a659186f00080ffd79ab114
lnewmanheggie <lnewmanheggie@gmail.com>
2021-02-24 11:54:24 -0600

    three routes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,51 @@
-var db = require("../models");
+const db = require("../models");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 module.exports = function(app) {
-
-  // get last workout
-  app.get("/api/workouts", function(req, res) {
     
-  });
-
-  // add exercise
-  app.put("/api/workouts/:id", function(req, res) {
-
-  });
-
   // create workout
   app.post("/api/workouts", function(req, res) {
-
+    db.Workout.create({ day: new Date().setDate(new Date().getDate()) })
+    .then(data => {
+        res.json(data);
+    })
+    .catch(({ message }) => {
+        console.log(message);
+    });
   });
 
-  // get workouts in range
-  app.get("/api/workouts/range", function(req, res) {
+  //get workouts
+  app.get("/api/workouts", function(req, res) {
+    db.Workout.find().sort({ _id: -1 }).limit(1)
+    .populate("exercises")
+    .then(data => {
+        console.log(data);
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+  });
+
+//   add exercise
+  app.put("/api/workouts/:id", (req, res) => {
+      const query = { _id: req.params.id };
+    db.Exercise.create(req.body)
+        .then(({ _id }) => db.Workout.findOneAndUpdate(query, { $push: { exercises: _id } }, { new: true }))
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+  });
+
+//   // get workouts in range
+//   app.get("/api/workouts/range", function(req, res) {
     
-  });
+//   });
 
 
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -6,7 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// require("./routes/apiRoutes.js")(app);
+app.use(logger("dev"));
+
+require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+
+
+
